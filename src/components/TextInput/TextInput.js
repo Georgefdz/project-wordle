4dd21c1db822d9dfb4cd95986c8836400ff653df
Georgefdz <georgfdz@gmail.com>
2024-07-22 17:39:36 -0600

    Add: minLength and maxLength to input. Remove: if statement and implement UpperCase directly on input

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -3,14 +3,14 @@ import React from 'react';
 function TextInput() {
 
   const [guess, setGuess] = React.useState('')
-  finalAnswer = guess.toUpperCase()
 
   function handleSubmit(event) {
     event.preventDefault();
-    if (finalAnswer.length > 5 || finalAnswer.length < 5) {
-      alert("Please enter a 5-letter word.")
-    }
-    console.info({ finalAnswer })
+    // if (finalAnswer.length > 5 || finalAnswer.length < 5) {
+    //   alert("Please enter a 5-letter word.")
+    //   return;
+    // }
+    console.info({ guess })
     setGuess('');
   }
 
@@ -18,8 +18,8 @@ function TextInput() {
   <>
     <form className="guess-input-wrapper" onSubmit={handleSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
-      <input id="guess-input" type="text" value={guess} onChange={(event) => {
-        setGuess(event.target.value)
+      <input id="guess-input" minLength={5} maxLength={5} type="text" value={guess} onChange={(event) => {
+        setGuess(event.target.value.toUpperCase())
       }}/>
     </form>
   </>
